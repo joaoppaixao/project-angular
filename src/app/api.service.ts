adf8e03f4e43acaeafe5002e7999046feaef3702
João Augusto Palma Paixão <joaoaugustopp@gmail.com>
2024-11-01 09:21:07 -0300

    Atualizei a API Service

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+private apiUrl = 'https://jsonplaceholder.typicode.com/todos';
+ constructor (private http: HttpClient) {}
 
-  constructor() { }
+ getTodos(): Observable<Todo[]>{
+  return this.http.get<Todo[]>(this.apiUrl)
+ }
+
+ addTodos(todo: Todo): Observable<Todo[]>{
+  return this.http.post<Todo[]>(this.apiUrl, todo);
+ }
+
+ updateTodos(todo: Todo): Observable<Todo[]>{
+  return this.http.put<Todo[]>('$(this.apiUrl)/$, todo.id)', todo);
+ }
+
+ deleteTodos(todo: Todo): Observable<Todo[]>{
+  return this.http.delete<Todo[]>('$(this.apiUrl)/$, todo.id)/$todo)');
+ }
 }
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService, Todo } from '../api.service';
 
 @Component({
   selector: 'app-todos',
   templateUrl: './todos.component.html',
   styleUrls: ['./todos.component.css']
 })
-export class TodosComponent {
+export class TodosComponent implements OnInit {
+  todos: Todo[] = [];
+  newTodo: Todo = { id: 0, title:'', completed: false};
+  constructor(private ApiService: ApiService) {}
 
+  ngOnInit(): void {
+    this.loadTodos();
+  }
+
+  loadTodos(){
+    this.ApiService.getTodos().subscribe(data => {
+      this.todos = data;
+    });
+
+    
+  }
 }
